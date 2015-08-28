b830c7b90281c04fdb27c84eab8d6860455d0201
Josh Malkinson <josh.malkinson@gmail.com>
2015-08-28 17:26:29 +0100

    Allow adduser route to POST to database

--- topknotch/routes/index.js
+++ topknotch/routes/index.js
@@ -27,4 +27,30 @@ router.get('/newuser', function(req, res) {
     res.render('newuser', { title: 'Add New User' });
 });
 
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes.
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to DB
+    collection.insert({
+        "username": userName,
+        "email": userEmail
+    }, function(err, doc) {
+        if (err) {
+            res.send('There was a problem adding the information to the database.');
+        } else {
+            res.redirect('userlist');
+        }
+    });
+});
+
 module.exports = router;
